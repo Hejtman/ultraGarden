ebd4ab809df58b8b853b22ac4c40017883e7ec25
Hejtman <hejtman2@centrum.cz>
2016-07-03 01:01:52 +0200

    improved ploting graphs

--- web/plot/graph.js
+++ web/plot/graph.js
@@ -1,6 +1,10 @@
 var chart = AmCharts.makeChart("chartdiv", {
     "type": "serial",
     "theme": "light",
+    "marginRight": 20,
+    "marginLeft": 20,
+    "autoMarginOffset": 10,
+    "mouseWheelZoomEnabled":true,
     "legend": {
         "useGraphSettings": true
     },
@@ -62,14 +66,30 @@ var chart = AmCharts.makeChart("chartdiv", {
         "valueField": "barel",
 		  "fillAlphas": 0
     }, {
+        "id": "v3",
         "valueAxis": "v3",
         "lineColor": "#FFAA00",
         "title": "balcony temperature in shadow",
         "valueField": "balcony",
 		  "fillAlphas": 0
     }],
+    "trendLines": [{
+        "finalDate": "2099",
+        "finalValue": 30,
+        "initialDate": "2000",
+        "initialValue": 30,
+        "lineColor": "#CC0000"
+    }, {
+        "finalDate": "2099",
+        "finalValue": 4,
+        "initialDate": "2000",
+        "initialValue": 4,
+        "lineColor": "#CC0000"
+    }],
 	 "chartCursor": {
-		  "cursorPosition": "mouse"
+		 "categoryBalloonDateFormat": "MM-DD JJ:NN",
+		  "cursorPosition": "mouse",
+		  "showNextAvailable": true
     },
     "categoryAxis": {
         "parseDates": true,
@@ -77,18 +97,32 @@ var chart = AmCharts.makeChart("chartdiv", {
         "axisColor": "#DADADA",
         "minorGridEnabled": true
     },
+    "valueScrollbar": {
+        "oppositeAxis": false,
+        "offset": 50,
+        "scrollbarHeight": 10
+    },
+	 "chartScrollbar": {
+        "graph": "v3",
+        "scrollbarHeight": 80,
+        "backgroundAlpha": 0,
+        "selectedBackgroundAlpha": 0.1,
+        "selectedBackgroundColor": "#888888",
+        "graphFillAlpha": 0,
+        "graphLineAlpha": 0.5,
+        "selectedGraphFillAlpha": 0,
+        "selectedGraphLineAlpha": 1,
+        "autoGridCount": true,
+        "color": "#AAAAAA"
+    },
     "categoryField": "date",
     "export": {
     	"enabled": true,
-		"position": "top-right"
-    },
+    }
 });
 
 chart.addListener("dataUpdated", zoomChart);
-zoomChart();
-
 
 function zoomChart(){
-    chart.zoomToIndexes(chart.dataProvider.length - 0, chart.dataProvider.length - 0);
+    chart.zoomToDates(new Date(new Date().setDate(new Date().getDate()-2)), new Date());
 }
-
