1cf27820cb0fd0a61ffc241e30d132c97bc80290
Michal Hejtmanek <hejtman2@centrum.cz>
2016-07-12 19:16:28 +0000

    server: small fix

--- server/utils/wifi_rebooter.sh
+++ server/utils/wifi_rebooter.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# /etc/crontab: */5 *   * * *   root    /usr/pi/bin/ultraGarden/server/utils/wifi_rebooter.sh
+# /etc/crontab: */5 *   * * *   root    /home/pi/ultraGarden/server/utils/wifi_rebooter.sh
 
 
 SERVER=192.168.10.1
 
 ping -c2 ${SERVER} > /dev/null
-if [ $? ]
+if [ $? -ne 0 ]
 then
     ifdown --force wlan0
     ifup wlan0
